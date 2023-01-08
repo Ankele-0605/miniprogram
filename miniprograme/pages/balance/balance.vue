17cc6fa898886b3691d39946bdc0b8c611f8a649
Ankele-0605 <2969811382@qq.com>
2023-01-08 17:09:42 +0800

    '新建了页面'

---
+++ miniprograme/pages/balance/balance.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<uni-nav-bar :fixed="true" status-bar left-icon="left" title="余额记录" @clickLeft="goBack" />
+		余额记录
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			goBack() {
+				uni.navigateBack({
+					delta: 1
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ miniprograme/pages/mytracks/mytracks.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<uni-nav-bar :fixed="true" status-bar left-icon="left" title="我的足迹" @clickLeft="goBack" />
+		我的足迹
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			goBack() {
+				uni.navigateBack({
+					delta: 1
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
