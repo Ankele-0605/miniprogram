cc35baa1bffa4c3b589c12c032be11e9a7d33bf0
Ankele-0605 <2969811382@qq.com>
2023-01-08 15:15:47 +0800

    '更新了我的页面'

--- miniprograme/pages/me/me.vue
+++ miniprograme/pages/me/me.vue
@@ -2,7 +2,28 @@
 	<view>
 		<uni-nav-bar :fixed="true" status-bar left-icon="home" title="用户中心" @clickLeft="toIndex" />
 		<view class="top">
-
+			<view class="topLeft">
+				<view class="avatar">
+					<image class="image" src="../../static/icon/avatar.png"></image>
+				</view>
+				<view class="wxuser">
+					<text class="text">微信用户</text>
+					<view class="refresh">
+						<image class="image" src="@/static/user/refresh.png"></image>
+						<text class="text">刷新</text>
+					</view>
+				</view>
+			</view>
+			<view class="topRight">
+				<view class="code">
+					<image class="payment" src="@/static/user/pay-code.png"></image>
+					<text class="text">付款码</text>
+				</view>
+				<view class="address">
+					<image class="image" src="@/static/user/address-white.png"></image>
+					<text class="text">收货地址</text>
+				</view>
+			</view>
 		</view>
 		<view class="content">
 			<view class="my">
@@ -30,38 +51,34 @@
 				</uni-grid>
 			</view>
 			<view class="order">
-				<uni-section>
-					<view class="title">
-						<text class="myOrder">我的订单</text>
-						<view class="more">
-							<text class="text">查看更多</text>
-							<uni-icons type="right"></uni-icons>
-						</view>
+				<view class="title">
+					<text class="myOrder">我的订单</text>
+					<view class="more">
+						<text class="text">查看更多</text>
+						<uni-icons type="right"></uni-icons>
 					</view>
-					<uni-grid :column="5" :show-border="false" :square="false" :highlight="true" @change="change">
-						<uni-grid-item v-for="(item ,index) in orderList" :index="index" :key="index">
-							<view class="grid-item-box" style="background-color: #fff;">
-								<image class="image" :src="item.url" mode="aspectFill" />
-								<text class="text">{{item.text}}</text>
-								<view v-if="item.badge" class="grid-dot">
-									<uni-badge :text="item.badge" :type="item.type" />
-								</view>
+				</view>
+				<uni-grid :column="5" :show-border="false" :square="false" :highlight="true" @change="change">
+					<uni-grid-item v-for="(item ,index) in orderList" :index="index" :key="index">
+						<view class="grid-item-box" style="background-color: #fff;">
+							<image class="image" :src="item.url" mode="aspectFill" />
+							<text class="text">{{item.text}}</text>
+							<view v-if="item.badge" class="grid-dot">
+								<uni-badge :text="item.badge" :type="item.type" />
 							</view>
-						</uni-grid-item>
-					</uni-grid>
-				</uni-section>
+						</view>
+					</uni-grid-item>
+				</uni-grid>
 			</view>
 			<view class="serve">
-				<uni-section>
-					<uni-grid :column="4" :show-border="false" :highlight="true" @change="change">
-						<uni-grid-item v-for="(item, index) in serveList" :index="index" :key="index">
-							<view class="grid-item-box" style="background-color: #fff;">
-								<image class="image" :src="item.url" mode="aspectFill" />
-								<text class="text">{{item.text}}</text>
-							</view>
-						</uni-grid-item>
-					</uni-grid>
-				</uni-section>
+				<uni-grid :column="4" :show-border="false" :square="false" :highlight="true" @change="change">
+					<uni-grid-item v-for="(item, index) in serveList" :index="index" :key="index">
+						<view class="grid-item-box" style="background-color: #fff;">
+							<image class="image" :src="item.url" mode="aspectFill" />
+							<text class="text">{{item.text}}</text>
+						</view>
+					</uni-grid-item>
+				</uni-grid>
 			</view>
 		</view>
 	</view>
@@ -135,7 +152,7 @@
 
 				serveList: [{
 						url: '/static/serve/delete.png',
-						text: '待付款',
+						text: '清理缓存',
 					},
 					{
 						url: '/static/serve/service.png',
@@ -180,7 +197,101 @@
 
 <style lang="less" scoped>
 	.top {
-		height: 200rpx;
+		display: flex;
+		justify-content: space-between;
+		height: 280rpx;
+		background: url('@/static/user/img-user-bg.png') center;
+		background-size: cover;
+
+		.topLeft {
+			display: flex;
+			align-items: center;
+
+			.avatar {
+				width: 60px;
+				height: 60px;
+				border-radius: 50%;
+				margin: 0 10px 0 20px;
+
+				.image {
+					width: 100%;
+					height: 100%;
+				}
+			}
+
+			.wxuser {
+				.text {
+					color: #fff;
+				}
+
+				.refresh {
+					display: inline-block;
+					width: 55px;
+					line-height: 18px;
+					text-align: center;
+					line-height: 20px;
+					margin-left: 5px;
+					border-radius: 15px;
+					border: 1px solid #fff;
+
+					.image {
+						width: 12px;
+						height: 12px;
+						vertical-align: middle;
+						margin-right: 4px;
+					}
+
+					.text {
+						font-size: 10px;
+						color: #fff;
+					}
+				}
+			}
+		}
+
+		.topRight {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+
+			.code {
+				margin-left: 40px;
+				margin-top: 20px;
+
+				.payment {
+					display: block;
+					width: 25px;
+					height: 25px;
+					margin-left: 3px;
+				}
+
+				.text {
+					font-size: 12px;
+					color: #fff;
+				}
+
+			}
+
+			.address {
+				margin-bottom: 15px;
+				background-color: #ff4544;
+				padding: 5px 8px;
+				border-radius: 32px 0 0 32px;
+				display: inline-block;
+
+				.image {
+					width: 20px;
+					height: 20px;
+					vertical-align: middle;
+					margin-right: 5px;
+				}
+
+				.text {
+					font-size: 14px;
+					color: #fff;
+				}
+			}
+		}
 	}
 
 	.content {
@@ -209,7 +320,7 @@
 
 				.num {
 					text-align: center;
-					margin: 5px 0;
+					margin-top: 5px;
 
 				}
 
@@ -217,45 +328,9 @@
 					text-align: center;
 
 					.image {
-						width: 30px;
-						height: 30px;
+						width: 20px;
+						height: 20px;
 						padding-right: 8px;
-						vertical-align: middle;
-					}
-
-					.text {
-						color: #515151;
-						font-size: 16px;
-						margin-top: 5px;
-					}
-				}
-			}
-
-
-		}
-
-		.canUse {
-			.grid-item-box {
-				flex: 1;
-				// position: relative;
-				/* #ifndef APP-NVUE */
-				display: flex;
-				/* #endif */
-				flex-direction: column;
-				align-items: center;
-				justify-content: center;
-				padding: 15px 0;
-
-				.num {
-					font-weight: 700;
-					color: #ffbc43;
-				}
-
-				.explain {
-					.image {
-						width: 15px;
-						height: 15px;
-						padding-right: 5px;
 						vertical-align: middle;
 					}
 
@@ -265,23 +340,66 @@
 						margin-top: 5px;
 					}
 				}
+			}
+
+
+		}
+
+		.canUse {
+			margin: 5px 10px 10px;
+
+			.grid-item-box {
+				flex: 1;
+				// position: relative;
+				/* #ifndef APP-NVUE */
+				display: flex;
+				/* #endif */
+				flex-direction: column;
+				align-items: center;
+				justify-content: center;
+				padding: 5px 0;
+				border-right: 1px solid #f4f4f4;
+
+				.num {
+					font-weight: 700;
+					color: #ffbc43;
+				}
+
+				.explain {
+					.image {
+						width: 12px;
+						height: 12px;
+						padding-right: 5px;
+						vertical-align: middle;
+					}
+
+					.text {
+						color: #515151;
+						font-size: 12px;
+						margin-top: 5px;
+					}
+				}
 
 			}
 
 		}
 
 		.order {
+			margin: 5px 10px 10px;
+
 			.title {
 				display: flex;
 				flex-direction: row;
 				justify-content: space-between;
+				background-color: #fff;
+				padding-top: 5px;
 
 				.myOrder {
-					margin-left: 15px;
+					padding-left: 15px;
 				}
 
 				.more {
-					margin-right: 15px;
+					padding-right: 10px;
 
 					.text {
 						color: #bbb;
@@ -307,8 +425,9 @@
 				}
 
 				.text {
-					font-size: 14px;
+					font-size: 12px;
 					margin-top: 5px;
+					color: #515151;
 				}
 
 				.grid-dot {
@@ -320,6 +439,9 @@
 		}
 
 		.serve {
+			margin: 0 10px;
+			border: 1px solid #f3f3f5;
+
 			/deep/.uni-section {
 				background-color: #f7f7f7;
 			}
@@ -333,17 +455,18 @@
 				flex-direction: column;
 				align-items: center;
 				justify-content: center;
-				padding: 15px 0;
+				padding: 13px 0;
 			}
 
 			.image {
-				width: 30px;
-				height: 30px;
+				width: 24px;
+				height: 24px;
 			}
 
 			.text {
-				font-size: 14px;
-				margin-top: 5px;
+				font-size: 12px;
+				margin-top: 10px;
+				color: #515151;
 			}
 		}
 	}
