e43db45c290d4e4f054f14dfbc00afc1b60a00b0
“ehahasan” <2438250147@qq.com>
2023-01-10 22:34:22 +0800

    提交购物车修改，数据库修改，收藏部分页面

--- miniprograme/pages/cart/cart.vue
+++ miniprograme/pages/cart/cart.vue
@@ -121,7 +121,7 @@
 				checkAll: false, //全选
 				selectDefault: "../../static/icon/uncheck.png", // 默认图标 
 				selectPitchOn: "../../static/icon/checked.png", // 选中图标
-
+	
 				isSelection: false, // 规格选择默认false
 				isCheckAll: false, // 购物车全选/反选默认false
 				numberValue: 0,
@@ -129,13 +129,13 @@
 				goods_num : 0,//总件数
 				adminShow: false, // 编辑选择默认false
 				ideld: [],
-
-
+	
+	
 			}
 		},
 		created() {
 			this.getcartlist();
-
+	
 		},
 		onShow() {
 			this.getcartlist();
@@ -163,8 +163,8 @@
 				this.cartlist[index].buynum = buynum;
 				this.totalPrice()
 			},
-
-
+	
+	
 			// 获得购物车商品数据
 			async getcartlist() {
 				var result = await axiosGet("/api/cart");
@@ -193,7 +193,7 @@
 				// setTimeout(() => {
 				// 	this.$router.go(0)
 				// }, 500)
-
+	
 			},
 			// 点击全选
 			chooseCheckAll() {
@@ -214,7 +214,7 @@
 			},
 			// 商品选中反选
 			chooseGoodsSelect(index) {
-
+	
 				let count = 0;
 				let selectedNum = 0;
 				if (this.cartlist[index].selected) {
@@ -236,7 +236,7 @@
 				}
 				this.totalPrice();
 			},
-
+	
 			
 			// 计算总价
 			totalPrice() {
@@ -259,7 +259,7 @@
 				}
 			
 			},
-
+	
 			// 编辑按钮
 			chooseSwitchover() {
 				this.adminShow = !this.adminShow
@@ -279,231 +279,5 @@
 </script>
 
 <style lang="less">
-	.scroll-Y {
-		// margin-top: 100rpx;
-		height: 1008rpx;
-		background-color: #f7f7f7;
-
-		// 头部（含编辑按钮）
-		.header {
-			width: 100%;
-			height: 80rpx;
-			display: flex;
-			flex-wrap: nowrap;
-			justify-content: space-between;
-			font-size: 30rpx;
-			padding: 20rpx;
-			box-sizing: border-box;
-			background-color: #ffffff;
-
-
-			.header-l {
-				color: lightslategray;
-				font-size: 30rpx;
-			}
-		}
-
-		.container {
-			margin-top: 35rpx;
-			background-color: #f7f7f7;
-
-			.empty {
-				margin-top: 50px;
-
-				.cart-emtpy {
-					width: 180rpx;
-					height: 180rpx;
-					background-color: #dedede;
-					margin: 0 auto;
-					display: flex;
-					/* 水平居中 */
-					justify-content: space-around;
-					/* 垂直居中 */
-					align-items: center;
-					box-sizing: border-box;
-					border-radius: 50%;
-
-					.image {
-						width: 50%;
-						height: 50%;
-						// margin: auto;
-					}
-
-				}
-
-				.cart-emtpy-font {
-					color: #b0b0b0;
-					line-height: 60px;
-					text-align: center;
-				}
-			}
-
-
-
-			.con-detail {
-				.selectHead {
-					// border: 1px solid gainsboro;
-					display: flex;
-					flex-wrap: nowrap;
-					// justify-content: space-between;
-					width: 100%;
-					height: 100rpx;
-					padding: 15px;
-					box-sizing: border-box;
-					background-color: #ffffff;
-
-					.select {
-						width: 50rpx;
-						margin-right: 10rpx;
-					}
-
-				}
-
-				.cart-card {
-
-					display: flex;
-					padding: 38rpx;
-					border-top: 1px solid #e4e4e4;
-					background-color: #ffffff;
-
-					.select-l {
-						position: relative;
-						width: 130rpx;
-						// background-color: aquamarine;
-
-						.select {
-							width: 50rpx;
-
-							position: absolute;
-							top: 35%;
-
-						}
-					}
-
-					.imag {
-						width: 200rpx;
-						// position: relative;
-						// background-color: olive;
-
-						.img {
-							width: 138rpx;
-							height: 138rpx;
-							position: relative;
-							top: 13%;
-
-
-						}
-					}
-
-					.message {
-						margin-left: 40rpx;
-
-						.biaoti {
-							height: 35rpx;
-							font-size: 28rpx;
-							overflow: hidden;
-							margin: 20rpx 0;
-						}
-
-						.guige {
-							font-size: 26rpx;
-							color: #b2b2b2;
-							margin-bottom: 40rpx;
-
-
-						}
-
-						.xia {
-							// margin-top: 20rpx;
-							display: flex;
-							justify-content: space-between;
-
-							.price {
-								font-size: 28rpx;
-								color: red;
-							}
-
-							.steper {
-								display: flex;
-								width: 80px;
-								font-size: 20px;
-							}
-						}
-					}
-
-
-				}
-
-			}
-
-
-			.radioh {
-				padding: 30rpx;
-				box-sizing: border-box;
-				height: 100rpx;
-			}
-		}
-	}
-
-	// 结算栏
-	.footer {
-		// background-color: antiquewhite;
-		border: 1px solid gainsboro;
-		display: flex;
-		flex-wrap: nowrap;
-		justify-content: space-between;
-		width: 100%;
-		height: 100rpx;
-		position: fixed;
-		bottom: 0;
-		padding: 15px;
-		box-sizing: border-box;
-
-		.quanxuan {
-			margin-left: -120rpx;
-		}
-
-		.quanxuanb {
-			margin-left: -300rpx;
-		}
-
-		.select {
-			width: 50rpx;
-			margin-right: 10rpx;
-		}
-
-		// 总计
-		.sum {
-			margin-left: -60px;
-			color: red;
-		}
-
-
-		// 结算（禁用状态）
-		.accounts {
-			width: 30%;
-			height: 80rpx;
-			line-height: 80rpx;
-			background-color: darkgray;
-			border-radius: 25px;
-			margin-top: -20rpx;
-			// border-radius: 15%;
-			color: white;
-			text-align: center;
-			position: relative;
-
-		}
-
-		//去结算
-		.abled-btn {
-			background-color: orangered !important;
-		}
-
-		.del-btn {
-			background-color: white !important;
-			border: 1px solid black;
-			color: black;
-		}
-
-	}
+	@import url('./cart.less');
 </style>
--- miniprograme/pages/mycollection/mycollection.vue
+++ miniprograme/pages/mycollection/mycollection.vue
@@ -1,7 +1,15 @@
 <template>
 	<view>
 		<uni-nav-bar :fixed="true" status-bar left-icon="left" title="我的收藏" @clickLeft="goBack" />
-		我的收藏
+		<view class="collnav">
+			<view :class="['collnav-goods',navSelect_l ? 'collnav-select':'']" @click="navclick_l">
+				商品
+			</view>
+			<view :class="['collnav-goods',navSelect_r ? 'collnav-select':'' ]" @click="navclick_r">
+				专题
+			</view>
+		</view>
+		<mycollecGoods></mycollecGoods>
 	</view>
 </template>
 
@@ -9,7 +17,8 @@
 	export default {
 		data() {
 			return {
-
+				navSelect_l:true,//导航选项卡左是否选中
+				navSelect_r:false,//导航选项卡右是否选中
 			}
 		},
 		methods: {
@@ -17,11 +26,22 @@
 				uni.navigateBack({
 					delta: 1
 				});
+			},
+			//导航选项卡左点击切换事件
+			navclick_l(){
+				this.navSelect_l=true;
+				this.navSelect_r=false;
+			},
+			//导航选项卡右点击事件
+			navclick_r(){
+				this.navSelect_r=true;
+				this.navSelect_l=false;
 			}
+		
 		}
 	}
 </script>
 
-<style>
-
+<style lang="less">
+	@import url('mycollection.less');
 </style>
