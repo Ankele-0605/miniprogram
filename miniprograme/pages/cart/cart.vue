9b0403d08032afdd9564bb4d8656a2f2136d6d7d
“ehahasan” <2438250147@qq.com>
2023-01-10 10:26:44 +0800

    修改购物车

--- miniprograme/pages/cart/cart.vue
+++ miniprograme/pages/cart/cart.vue
@@ -60,9 +60,18 @@
 								</view>
 								<!-- 步进器 -->
 								<view class="steper">
-									<!-- <uni-number-box :value="numberValue" @change="change" /> -->
-									<van-stepper min="0" button-size="16px" input-width="20px" :value="numberValue"
-										@change="onStepperChange" :data-name="item.name" :data-id="item.id" />
+									<!-- <van-stepper min="0" button-size="16px" input-width="20px" :value="numberValue"
+										@change="onStepperChange" :data-name="item.name" :data-id="item.id" /> -->
+									<view class="num-minus" @click="chooseMinus(index)">
+										-
+									</view>
+									<view class="input-price">
+										{{item.buynum}}
+									</view>
+									<view class="num-add" @click="chooseAddNum(index)">
+										+
+									</view>
+
 								</view>
 							</view>
 						</view>
@@ -116,7 +125,8 @@
 				isSelection: false, // 规格选择默认false
 				isCheckAll: false, // 购物车全选/反选默认false
 				numberValue: 0,
-				sumMoney: 0.00, //价格总计
+				sumMoney: 0, //价格总计
+				goods_num : 0,//总件数
 				adminShow: false, // 编辑选择默认false
 				ideld: [],
 
@@ -128,7 +138,6 @@
 
 		},
 		onShow() {
-			// console.log("1111112");
 			this.getcartlist();
 		},
 		methods: {
@@ -137,6 +146,25 @@
 					url: "/pages/index/index"
 				})
 			},
+			// 增加数量
+			chooseAddNum(index) {
+				let buynum = this.cartlist[index].buynum; // 获取当前数量
+				buynum = buynum + 1; // 每点击一次加1
+				this.cartlist[index].buynum = buynum; // 数量
+				this.totalPrice()
+			},
+			// 减少数量
+			chooseMinus(index) {
+				let buynum = this.cartlist[index].buynum; // 获取当前数量
+				if (buynum <= 1) {
+					return
+				}
+				buynum = buynum - 1; // 每点击一次加1
+				this.cartlist[index].buynum = buynum;
+				this.totalPrice()
+			},
+
+
 			// 获得购物车商品数据
 			async getcartlist() {
 				var result = await axiosGet("/api/cart");
@@ -146,14 +174,13 @@
 					// 添加selected字段
 					for (let i = 0; i < this.cartlist.length; i++) {
 						this.cartlist[i].selected = false;
-						this.cartlist[i].buynum = 0;
+						this.cartlist[i].buynum = 1;
 					}
-					console.log(this.cartlist, "......2");
 				}
+				this.totalPrice();
 			},
 			// 删除购物车商品
 			async chooseGoodsDelete() {
-				console.log("lllllllll")
 				this.getdelid();
 				var result = await axiosPost("/api/delcart", {
 					ideld: this.ideld
@@ -162,6 +189,7 @@
 					console.log(result.data);
 				}
 				this.getcartlist();
+				this.isCheckAll = false;
 				// setTimeout(() => {
 				// 	this.$router.go(0)
 				// }, 500)
@@ -174,7 +202,6 @@
 				console.log(this.isCheckAll, "this.isCheckAll");
 				if (isCheckAll) {
 					this.isCheckAll = false;
-					this.sumMoney = 0.00;
 					this.isDisabled = true;
 				} else {
 					this.isCheckAll = true;
@@ -183,22 +210,19 @@
 				for (let i = 0; i < this.cartlist.length; i++) {
 					this.cartlist[i].selected = this.isCheckAll
 				}
+				this.totalPrice()
 			},
 			// 商品选中反选
 			chooseGoodsSelect(index) {
 
 				let count = 0;
 				let selectedNum = 0;
-				// let goods = this.cartlist; // 当前商品数组
-
 				if (this.cartlist[index].selected) {
 					this.cartlist[index].selected = false; // 改变当前商品状态
-					// selectedNum--
 					this.isCheckAll = false
 				} else {
 					this.cartlist[index].selected = true;
 					let shopGoodsNum = this.cartlist.length;
-					// let selectedNum = 0;
 					for (var i in this.cartlist) {
 						if (this.cartlist[i].selected) {
 							selectedNum++
@@ -206,58 +230,46 @@
 					}
 					if (selectedNum == this.cartlist.length) {
 						this.isCheckAll = true;
-					} else if (selectedNum == 0) {
-						this.isCheckAll = false;
-						this.sumMoney = 0;
 					} else {
 						this.isCheckAll = false;
 					}
 				}
+				this.totalPrice();
 			},
 
-			//当步进条发生改变的时候执行的
-			onStepperChange(e) {
-				let sumMoney = 0;
-				this.cartlist.forEach(item => {
-					if (item.name === e.currentTarget.dataset.name) {
-						item.buynum = e.detail;
-						console.log(this.cartlist)
-						if (item.selected) {
-							for (var i = 0; i < this.cartlist.length; i++) {
-								sumMoney += this.cartlist[i].price * this.cartlist[i].buynum;
-							}
-
-						} else {
-							item.selected = true;
-							for (var i = 0; i < this.cartlist.length; i++) {
-								sumMoney += this.cartlist[i].price * this.cartlist[i].buynum;
-							}
-						}
-
-
-					};
-				})
+			
+			// 计算总价
+			totalPrice() {
+				let sumMoney = 0;//总金额
+				let goods_num = 0;//总件数
+				for (var i = 0; i < this.cartlist.length; i++) {
+					if (this.cartlist[i].selected) {
+						sumMoney += this.cartlist[i].price * this.cartlist[i].buynum;
+						goods_num+=this.cartlist[i].buynum;
+					}
+				}
 				this.sumMoney = parseFloat(sumMoney).toFixed(2);
-				console.log(this.sumMoney, 'sumMoney');
-				if (this.sumMoney == 0) {
+				this.goods_num = goods_num;
+				// console.log(this.sumMoney,'this.sumMoney')
+				// console.log(this.goods_num,'this.goods_num')
+				if(this.goods_num == 0) {
 					this.isDisabled = true;
 				} else {
 					this.isDisabled = false;
 				}
-				console.log(this.isDisabled, 'isDisabled');
-
+			
 			},
+
 			// 编辑按钮
 			chooseSwitchover() {
 				this.adminShow = !this.adminShow
 			},
 			// 获得选中的id数组
 			getdelid() {
+				this.ideld = [];
 				this.cartlist.forEach(item => {
 					if (item.selected) {
 						this.ideld.push(item.id);
-						// item.cart = false;
-						// console.log(this.cartlist,'this.cartlist')
 					}
 					console.log(this.ideld, "this.ideld");
 				})
@@ -297,6 +309,7 @@
 
 			.empty {
 				margin-top: 50px;
+
 				.cart-emtpy {
 					width: 180rpx;
 					height: 180rpx;
@@ -411,8 +424,9 @@
 							}
 
 							.steper {
-
-								font-size: 12px;
+								display: flex;
+								width: 80px;
+								font-size: 20px;
 							}
 						}
 					}
