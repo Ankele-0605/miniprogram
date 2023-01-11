b6cafcc110e73e3164e3595b9ef19a42469aa5c7
Ankele-0605 <2969811382@qq.com>
2023-01-11 14:48:21 +0800

    '修改了余额记录页面'

--- miniprograme/pages/balance/balance.vue
+++ miniprograme/pages/balance/balance.vue
@@ -6,7 +6,7 @@
 			<image class="rule" src="@/static/serve/balance/balance-icon.png" mode=""></image>
 			<view class="count">
 				<text class="title">账户余额(元)</text>
-				<text class="num">{{countNum}}</text>
+				<text class="num">{{ countNum }}</text>
 				<view class="add" @click="toVoucherPage">
 					<text class="text">充值</text>
 				</view>
@@ -17,119 +17,118 @@
 		</view>
 
 		<view class="date">
-			<uni-calendar class="uni-calendar--hook" :selected="info.selected" :showMonth="false"
-				@monthSwitch="monthSwitch" />
+			<uni-calendar class="uni-calendar--hook" :selected="info.selected" :showMonth="false" />
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				countNum: "0",
-				info: {
-					lunar: true,
-					range: true,
-					insert: false,
-					selected: []
-				}
-			}
-		},
-
-		methods: {
-			goBack() {
-				uni.navigateBack({
-					delta: 1
-				});
-			},
-			toVoucherPage() {
-				uni.navigateTo({
-					url: '/pages/voucherCenter/voucherCenter',
-					animationType:'none'
-				});
+export default {
+	data() {
+		return {
+			countNum: "0",
+			info: {
+				lunar: true,
+				range: true,
+				insert: false,
+				selected: []
 			}
 		}
+	},
+
+	methods: {
+		goBack() {
+			uni.navigateBack({
+				delta: 1
+			});
+		},
+		toVoucherPage() {
+			uni.navigateTo({
+				url: '/pages/voucherCenter/voucherCenter',
+				animationType: 'none'
+			});
+		}
 	}
+}
 </script>
 
 <style lang="less">
-	.main {
-		height: 100vh;
+.main {
+	height: 100vh;
 
-		.top {
-			width: 750rpx;
-			height: 300rpx;
-			position: relative;
-			font-size: 12px;
+	.top {
+		width: 750rpx;
+		height: 300rpx;
+		position: relative;
+		font-size: 12px;
 
-			.image {
-				width: 100%;
-				height: 100%;
-				position: absolute;
-				z-index: -999;
-			}
-
-			.rule {
-				width: 20px;
-				height: 20px;
-				position: absolute;
-				right: 8px;
-				top: 15px;
-			}
-
-			.count {
-				height: 100%;
-				display: flex;
-				flex-direction: column;
-				align-items: center;
-
-				.title {
-					color: #fff;
-					margin-top: 24px;
-				}
-
-				.num {
-					color: #fff;
-					font-size: 30px;
-					margin: 10px;
-				}
-
-				.add {
-					width: 45px;
-					line-height: 25px;
-					text-align: center;
-					line-height: 25px;
-					border-radius: 15px;
-					border: 1px solid #fff;
-
-					.text {
-						color: #fff;
-
-					}
-				}
-			}
-
+		.image {
+			width: 100%;
+			height: 100%;
+			position: absolute;
+			z-index: -999;
 		}
 
-		.addImage {
-			margin: 8px 0;
-			background-color: #f7f7f7;
-
-			.image {
-				width: 100%;
-			}
+		.rule {
+			width: 20px;
+			height: 20px;
+			position: absolute;
+			right: 8px;
+			top: 15px;
 		}
 
-		.date {
-			.uni-calendar__box {
-				display: none;
+		.count {
+			height: 100%;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+
+			.title {
+				color: #fff;
+				margin-top: 24px;
 			}
 
-			.uni-calendar__backtoday {
-				display: none;
+			.num {
+				color: #fff;
+				font-size: 30px;
+				margin: 10px;
+			}
+
+			.add {
+				width: 45px;
+				line-height: 25px;
+				text-align: center;
+				line-height: 25px;
+				border-radius: 15px;
+				border: 1px solid #fff;
+
+				.text {
+					color: #fff;
+
+				}
 			}
 		}
 
 	}
+
+	.addImage {
+		margin: 8px 0;
+		background-color: #f7f7f7;
+
+		.image {
+			width: 100%;
+		}
+	}
+
+	.date {
+		.uni-calendar__box {
+			display: none;
+		}
+
+		.uni-calendar__backtoday {
+			display: none;
+		}
+	}
+
+}
 </style>
